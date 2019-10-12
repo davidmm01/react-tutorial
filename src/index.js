d6a8e216287187180af49b8625ca060e3f91ab97
David Michieli <d.m.michieli@gmail.com>
2019-10-12 16:21:31 +1100

    learn about state

--- src/index.js
+++ src/index.js
@@ -2,18 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// State
+// Components use state to 'remember things'
+// React components can have state by setting this.state in their constructors
+
+
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+  constructor(props) {
+    // super() must be called when defining the constructor of a subclass
+    // all react component classes that have a constructor should start it with a super(props) call
+    super(props);  
+    this.state = {
+      value: null,
+    };
   }
+
+  render() {
+    return (
+      // Note we must pass a FUNCTION (hence () => ).  This way react only
+      // calls this function after a click.  If we just passed the alert 
+      // without the function, the alert would fire each time the component 
+      // is rendered
+      <button 
+        className="square" 
+        // calling this.setState from the onClick tells react to re-render the square whenever the button is clicked
+        // when you call setState in a component, react automatically updates the child components inside of it too
+        onClick={() => this.setState({value: 'X'})} 
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
   
 class Board extends React.Component {
   renderSquare(i) {
+    // we pass the prop "value" from the parent Board to the child Square
     return <Square value={i} />;
   }
 
